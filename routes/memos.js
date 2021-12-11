94cf401070d7ec53f42fb78a6043f9e0e1cf62dd
Masashi Itabashi <masa.itabashi@gmail.com>
2021-12-11 18:40:35 +0900

    削除

--- routes/memos.js
+++ routes/memos.js
@@ -11,18 +11,34 @@ router.get('/new', authenticationEnsurer, (req, res, next) => {
 
 router.post('/', authenticationEnsurer, (req, res, next) => {
   const updatedAt = new Date();
-  Memo.create({
-    memoId: Memo.memoId,
-    programName: req.body.programName.slice(0, 255) || '（番組名未設定）',
-    personality:
-      req.body.personality.slice(0, 255) || '（パーソナリティ未設定）',
-    airtime: req.body.airtime.slice(0, 255) || '（放送時間未設定）',
-    impression: req.body.impression,
-    createdBy: req.user.id,
-    updatedAt: updatedAt,
-  }).then(memo => {
-    res.redirect('/'); //トップ画面に転送
-  });
+
+  switch (req.url) {
+    //登録
+    case '/':
+      Memo.create({
+        memoId: Memo.memoId,
+        programName: req.body.programName.slice(0, 255) || '（番組名未設定）',
+        personality:
+          req.body.personality.slice(0, 255) || '（パーソナリティ未設定）',
+        airtime: req.body.airtime.slice(0, 255) || '（放送時間未設定）',
+        impression: req.body.impression,
+        createdBy: req.user.id,
+        updatedAt: updatedAt,
+      }).then(memo => {
+        res.redirect('/'); //トップ画面に転送
+      });
+      break;
+
+    //削除
+    case '/?delete=1':
+      Memo.findByPk(req.body.memoId)
+        .then(memo => {
+          memo.destroy();
+        })
+        .then(() => {
+          res.redirect('/'); //トップ画面に転送
+        });
+  }
 });
 
 module.exports = router;
