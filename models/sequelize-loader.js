1f0139316503d43c79f473184a5bda7613ce496b
Masashi Itabashi <masa.itabashi@gmail.com>
2021-12-19 16:15:11 +0900

    Heroku対応

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -2,8 +2,23 @@
 
 //https://sequelize.org/master/index.html
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('postgres://postgres:postgres@db/radio-memo');
 
+const dialectOptions = {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
+};
+const sequelize = process.env.DATABASE_URL
+  ? // 本番環境
+    new Sequelize(process.env.DATABASE_URL, {
+      logging: false,
+      dialectOptions,
+    })
+  : // 開発環境
+    new Sequelize('postgres://postgres:postgres@db/radio-memo', {
+      logging: false,
+    });
 module.exports = {
   sequelize,
   DataTypes,
