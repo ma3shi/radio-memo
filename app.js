2d7c5575562cb627b3ecd9aeeea194ccb45fb6d2
Masashi Itabashi <masa.itabashi@gmail.com>
2021-12-19 19:16:49 +0900

    Herokuの環境変数を利用できるように修正

--- app.js
+++ app.js
@@ -45,7 +45,9 @@ passport.use(
     {
       clientID: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: 'http://localhost:8000/auth/github/callback',
+      callbackURL: process.env.HEROKU_URL
+        ? process.env.HEROKU_URL + 'auth/github/callback'
+        : 'http://localhost:8000/auth/github/callback',
     },
     function (accessToken, refreshToken, profile, done) {
       //process.nextTickで登録する関数は処理に時間がかかるデータベースへの保存する処理を記載
